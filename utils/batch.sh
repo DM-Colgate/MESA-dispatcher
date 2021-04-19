c45be1d1f0de1e544d088df25109f64f9605fbb9
deionizedoatmeal <ian.k.bania@gmail.com>
2021-04-18 22:14:46 -0700

    added example for batch script

--- utils/batch.sh
+++ utils/batch.sh
@@ -4,30 +4,18 @@
 
 echo "Dispatching feral hogs... please prepare for deallocation."
 
-# AA: m_chi = 1d3 and rho_chi = 1d6
-deploy AA 1d0 1d3 1d6 16
-deploy AA 3d0 1d3 1d6 16
-deploy AA 3d0 1d3 1d6 16
-deploy AA 5d0 1d3 1d6 16
-deploy AA 1d1 1d3 1d6 16
-deploy AA 3d1 1d3 1d6 16
-deploy AA 5d1 1d3 1d6 16
-deploy AA 1d2 1d3 1d6 16
-deploy AA 3d2 1d3 1d6 16
-deploy AA 5d2 1d3 1d6 16
-deploy AA 1d3 1d3 1d6 16
-
-# AB: m_chi = 1d3 and rho_chi = 1d7
-deploy AB 1d0 1d3 1d7 16
-deploy AB 3d0 1d3 1d7 16
-deploy AB 3d0 1d3 1d7 16
-deploy AB 5d0 1d3 1d7 16
-deploy AB 1d1 1d3 1d7 16
-deploy AB 3d1 1d3 1d7 16
-deploy AB 5d1 1d3 1d7 16
-deploy AB 1d2 1d3 1d7 16
-deploy AB 3d2 1d3 1d7 16
-deploy AB 5d2 1d3 1d7 16
-deploy AB 1d3 1d3 1d7 16
+# AA: M_* = 1d1 -> 1d3, m_chi = 1d10, rho_chi = 1d16, and 16 threads
+deploy AA MESA-DM 1d1 1d10 1d16 16
+deploy AA MESA-DM 2d1 1d10 1d16 16
+deploy AA MESA-DM 3d1 1d10 1d16 16
+deploy AA MESA-DM 5d1 1d10 1d16 16
+deploy AA MESA-DM 7d1 1d10 1d16 16
+deploy AA MESA-DM 1d2 1d10 1d16 16
+deploy AA MESA-DM 2d2 1d10 1d16 16
+deploy AA MESA-DM 3d2 1d10 1d16 16
+deploy AA MESA-DM 5d2 1d10 1d16 16
+deploy AA MESA-DM 5d2 1d10 1d16 16
+deploy AA MESA-DM 7d2 1d10 1d16 16
+deploy AA MESA-DM 1d3 1d10 1d16 16
 
 echo $(qstat)
